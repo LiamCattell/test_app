d2ec412190b11520b6164398506d9da2bde189f6
LiamCattell <liamccattell@gmail.com>
2016-01-31 11:54:38 +0000

    Try Flask request

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-	return render_template('jobs_vs_prices.html')
+    if request.method == 'GET':
+        return render_template('jobs_vs_prices.html')
+    else:
+        #request was a POST
+        return render_template('jobs_vs_prices_graph.html')
 	
 if __name__ == "__main__":
 	app.run()
