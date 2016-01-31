0f1abb021fb2fc478a0fd18c506d7b186a215e27
LiamCattell <liamccattell@gmail.com>
2016-01-31 13:50:48 +0000

    Simple plot

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from plotchoropleth import choropleth_usa
 
 app = Flask(__name__)
 
@@ -9,8 +10,13 @@ def index():
 
 @app.route('/jobs_vs_prices', methods=['POST'])
 def jobs_vs_prices():
-    script = '<!-- ff -->'
-    div = '<h3>Backend working</h3>'
+    #script = '<!-- ff -->'
+    #div = '<h3>Backend working</h3>'
+
+    scores = {'tx': 3.4, 'ca': 0.6, 'nc': 2.1}
+    
+    script, div = choropleth_usa(scores)
+
     return render_template('jobs_vs_prices_graph.html', script=script, div=div)
 	
 if __name__ == "__main__":
