86d9da8fc55af008b17541666c41797f9abc81d6
LiamCattell <liamccattell@gmail.com>
2016-01-31 14:21:55 +0000

    Try plotting directly from script

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 from flask import Flask, render_template, request
-from plotchoropleth import choropleth_usa
-from bokeh.plotting import figure
-from bokeh.embed import components
-
 
 app = Flask(__name__)
 
@@ -13,18 +9,12 @@ def index():
 
 @app.route('/jobs_vs_prices', methods=['POST'])
 def jobs_vs_prices():
-    #script = '<!-- ff -->'
-    #div = '<h3>Backend working</h3>'
-
-    #scores = {'tx': 3.4, 'ca': 0.6, 'nc': 2.1}
-    script, div = choropleth_usa({})
-
+    script_f = open('test_plot_script.txt', 'r')
+    script = script_f.read()
     
-#    plot = figure(title='Data from Quandle WIKI set',
-#                  x_axis_label='date',
-#                  x_axis_type='datetime')
-#                  
-#    script, div = components(plot)
+    div_f = open('test_plot_div.txt', 'r')
+    div = div_f.read()
+
 
     return render_template('jobs_vs_prices_graph.html', script=script, div=div)
 	
