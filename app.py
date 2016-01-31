1cccd7c44359ea84993e5a2ff3a848cd0fedcdda
LiamCattell <liamccattell@gmail.com>
2016-01-31 11:46:27 +0000

    First render template attempt

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Hello again, this works!'
+	return render_template('jobs_vs_prices.html')
 	
 if __name__ == "__main__":
 	app.run()
