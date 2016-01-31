19c2e38d2436c0715226f25a74444371eafb31de
LiamCattell <liamccattell@gmail.com>
2016-01-31 15:55:09 +0000

    Updated html doctype

--- createstaticplots.py
+++ createstaticplots.py
@@ -2,6 +2,10 @@ from getdata import load_jobs, get_average_prices_data
 from getscores import get_state_scores
 from plotchoropleth import choropleth_usa
 
+"""
+Generate (and save) choropleths of available jobs (scaled by house prices)
+"""
+
 prices = get_average_prices_data()
 
 profession_keys = ['data-science', 'financial-services', 'information-technology', 'mobile-app']
