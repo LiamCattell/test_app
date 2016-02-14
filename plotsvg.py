c01149b71c7f1690928ed11ddf11e4cf2af157a4
LiamCattell <liamccattell@gmail.com>
2016-02-14 10:26:45 +0000

    Plot svg dynamically without bs4

--- plotsvg.py
+++ plotsvg.py
@@ -1,35 +1,26 @@
-from bs4 import BeautifulSoup
+import xml.etree.ElementTree as et
 
 def choropleth_svg(scores):
-    # Load the SVG map
-    svg = open('data/us_counties.svg', 'r').read()
-    soup = BeautifulSoup(svg, 'xml')
-    
-    # Find counties
-    paths = soup.findAll('path')
+    tree = et.parse('data/us_counties.svg')
+    root = tree.getroot()
     
     # Map colors
     colors = ["#F1EEF6", "#D4B9DA", "#C994C7", "#DF65B0", "#DD1C77", "#980043"]
     
     path_style = 'font-size:12px;fill-rule:nonzero;stroke:#FFFFFF;stroke-opacity:1;'
     path_style += 'stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;'
-    path_style += 'stroke-linecap:butt;marker-start:none;stroke-linejoin:bevel;fill:'
-    
-    # Color the counties
-    for p in paths:
-        if p['id'] not in ['State_Lines', 'separator']:
-            # pass
+    path_style += 'stroke-linecap:butt;marker-start:none;stroke-linejoin:bevel;fill:'  
+        
+    for p in root.findall('{http://www.w3.org/2000/svg}path'):
+        if p.attrib['id'] not in ['State_Lines', 'separator']:
             try:
-                fips = (int(p['id'][:2]), int(p['id'][2:]))
+                fips = (int(p.attrib['id'][:2]), int(p.attrib['id'][2:]))
+                ind = min(int(scores[fips]*5), 5)
+                p.set('style', path_style + colors[ind])
             except:
                 continue
-                 
-            ind = min(int(scores[fips]*5), 5)
-            color = colors[ind]
-            p['style'] = path_style + color
-    
-#    with open('static/test_counties.svg', 'w') as f:
-#        f.write(soup.prettify())
-        #f.write(unicode(soup.body.next))
+            
+          
+    tree.write('static/test_counties.svg')
         
-    return soup.prettify()
+    return
--- test_xml.py
+++ test_xml.py
@@ -1,14 +1,34 @@
 import xml.etree.ElementTree as et
+from getdata import load_houseprices
+from getscores import get_houseprices_scores
+
+scores = get_houseprices_scores(load_houseprices())
 
 tree = et.parse('data/us_counties.svg')
 root = tree.getroot()
+
+# Map colors
+colors = ["#F1EEF6", "#D4B9DA", "#C994C7", "#DF65B0", "#DD1C77", "#980043"]
 
 path_style = 'font-size:12px;fill-rule:nonzero;stroke:#FFFFFF;stroke-opacity:1;'
 path_style += 'stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;'
 path_style += 'stroke-linecap:butt;marker-start:none;stroke-linejoin:bevel;fill:'
 
+#for p in root.findall('{http://www.w3.org/2000/svg}path'):
+#    p.set('style', path_style+'#C994C7')
+#    #print p.attrib['style']   
+    
 for p in root.findall('{http://www.w3.org/2000/svg}path'):
-    p.set('style', path_style+'#C994C7')
-    print p.attrib['style']
+    if p.attrib['id'] not in ['State_Lines', 'separator']:
+        # pass
+        try:
+            fips = (int(p.attrib['id'][:2]), int(p.attrib['id'][2:]))
+        except:
+            continue
+             
+        ind = min(int(scores[fips]*5), 5)
+        color = colors[ind]
+        p.set('style', path_style + color)
+        
       
 tree.write('test_counties2.svg')
